9e42cb72f1abf76c08f32a2aa81ae2a99388b287
Addi Mohammed Said <Addi_Mohammed_Said@outlook.fr>
2020-07-14 21:28:43 +0100

    + authguard admin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,6 +5,7 @@ import { ProductFormComponent } from "./comp/product-form/product-form.component
 import { AdminProductsComponent } from "./comp/admin-products/admin-products.component";
 import { AdminOrdersComponent } from "./comp/admin-orders/admin-orders.component";
 import { AuthGuardService } from "../shared/services/auth-guard.service";
+import { AdminAuthGuardService } from "./services/admin-auth-guard.service";
 
 @NgModule({
   imports: [
@@ -21,7 +22,7 @@ import { AuthGuardService } from "../shared/services/auth-guard.service";
       {
         path: "admin/products",
         component: AdminProductsComponent,
-        canActivate: [AuthGuardService],
+        canActivate: [AuthGuardService, AdminAuthGuardService],
       },
       {
         path: "admin/orders",
--- src/app/admin/services/admin-auth-guard.service.ts
+++ src/app/admin/services/admin-auth-guard.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { AppUser } from "./../../shared/models/app-user";
+import { AuthService } from "./../../shared/services/auth.service";
+import { RouterStateSnapshot } from "@angular/router";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AdminAuthGuardService {
+  appUser: AppUser;
 
-  constructor() { }
+  constructor(private _auth: AuthService) {}
+
+  canActivate(): Observable<boolean> {
+    return this._auth.appUser$.pipe(
+      map((appUser) => {
+        console.log(appUser.isAdmin);
+        return appUser.isAdmin;
+      })
+    );
+  }
 }
